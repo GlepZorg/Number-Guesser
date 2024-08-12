a3b373d5ec41f43303c10944da64effd3dcabfb0
Alexis Gonzalez <a.rgonzalez0951@gmail.com>
2024-08-11 21:54:05 -0700

    update script code

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
+
+const generateTarget = () => {
+  return Math.floor(Math.random() * 9);
+};
+
+const compareGuesses = (userGuess, computerGuess, targetNumber) => {
+  const userDifference = Math.abs(userGuess - targetNumber);
+  const computerDifference = Math.abs(computerGuess - targetNumber);
+  if (userDifference <= computerDifference) {
+    return true;
+  } else {
+    return false;
+  }
+};
+const updateScore = (winner) => {
+  if (winner === "human") {
+    humanScore++;
+  } else if (winner === "computer") {
+    computerScore++;
+  }
+};
+
+const advanceRound = () => {
+  currentRoundNumber++;
+};
