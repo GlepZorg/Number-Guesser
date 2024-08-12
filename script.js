8c8a27bbc67013525ad902ecdd1a47ff0815eb75
Alexis Gonzalez <a.rgonzalez0951@gmail.com>
2024-08-11 22:00:47 -0700

    update script

--- script.js
+++ script.js
@@ -3,16 +3,23 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 const generateTarget = () => {
-  return Math.floor(Math.random() * 9);
+  return Math.floor(Math.random() * 10);
+};
+const getAbsoluteDistance = (value1, value2) => {
+  return Math.abs(value1 - value2);
 };
 
 const compareGuesses = (userGuess, computerGuess, targetNumber) => {
-  const userDifference = Math.abs(userGuess - targetNumber);
-  const computerDifference = Math.abs(computerGuess - targetNumber);
-  if (userDifference <= computerDifference) {
-    return true;
+  if (userGuess >= 0 && userGuess <= 10) {
+    const userDifference = getAbsoluteDistance(userGuess, targetNumber);
+    const computerDifference = getAbsoluteDistance(computerGuess, targetNumber);
+    if (userDifference <= computerDifference) {
+      return true;
+    } else {
+      return false;
+    }
   } else {
-    return false;
+    alert("Number is out of range!");
   }
 };
 const updateScore = (winner) => {
